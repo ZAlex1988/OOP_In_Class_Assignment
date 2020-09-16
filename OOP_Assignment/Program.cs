131016dee479fa10a8d16170a6eb8186b672bfd6
Alex Zhideev <ZAlex1988@gmail.com>
2020-09-15 21:58:10 -0400

    OOP In Class Assignment

---
+++ OOP_Assignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Introduction
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Problem statement 1");
+            // Problem statement 1
+            Bike mybike = new Bike();
+
+            mybike.setNumberOfGears(6);
+            Console.WriteLine("Is my bike registered : " + mybike.verifyRegistrationNumber());
+           
+            Car mycar = new Car();
+            mycar.setModelYear(1979);
+
+            Console.WriteLine("Is my car antic : " + mycar.isAntic());
+
+            Console.WriteLine();
+            Console.WriteLine("Problem statement 2");
+            // Problem statement 2
+
+            mycar.Owner = new MyPerson("Alex", "Zhideev");
+
+            Console.WriteLine("Current owner : " + mycar.Owner.getOwnerName());
+
+            mycar.changeOwner(new MyPerson("Daniel", "Smith"));
+            
+            Console.WriteLine("New owner : " + mycar.Owner.getOwnerName());
+
+            Console.ReadKey();
+
+           
+        }
+    }
+}
